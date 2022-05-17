789914c34bf64effadc535456f0adf9006ad78d9
Kimminju0831 <@.com>
2022-05-17 16:12:21 +0900

    포스트 목록 페이지와 포스트 상세 페이지에 태그 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -3,7 +3,7 @@ from urllib import response
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class TestView(TestCase):
     def setUp(self):
@@ -14,6 +14,10 @@ class TestView(TestCase):
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
         
+        self.tag_python_kor = Tag.objects.create(name='파이썬 공부', slug='파이썬-공부')
+        self.tag_python = Tag.objects.create(name='python', slug='python')
+        self.tag_hello = Tag.objects.create(name='hello', slug='hello')
+        
         #1. 게시물이 2개 있다면
         self.post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
@@ -21,6 +25,7 @@ class TestView(TestCase):
             category = self.category_programming,
             author=self.user_trump
         )
+        self.post_001.tags.add(self.tag_hello)
         
         self.post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
@@ -34,6 +39,9 @@ class TestView(TestCase):
             content = 'category가 없을 수도 있죠',
             author=self.user_obama
         )
+        
+        self.post_003.tags.add(self.tag_python_kor)
+        self.post_003.tags.add(self.tag_python)
         
     def test_category_page(self):
         response = self.client.get(self.category_programming.get_absolute_url())
@@ -69,14 +77,26 @@ class TestView(TestCase):
             post_001_card = main_area.find('div', id='post-1')
             self.assertIn(self.post_001.title, post_001_card.text)
             self.assertIn(self.post_001.category.name, post_001_card.text)
+            self.assertIn(self.post_001.author.username.upper(), post_001_card.text)
+            self.assertIn(self.tag_hello.name, post_001_card.text)
+            self.assertNotIn(self.tag_python.name, post_001_card.text)
+            self.assertNotIn(self.tag_python_kor.name, post_001_card.text)
             
             post_002_card = main_area.find('div', id='post-2')
             self.assertIn(self.post_002.title, post_002_card.text)
             self.assertIn(self.post_002.category.name, post_002_card.text)
+            self.assertIn(self.post_002.author.username.upper(), post_002_card.text)
+            self.assertNotIn(self.tag_hello.name, post_002_card.text)
+            self.assertNotIn(self.tag_python.name, post_002_card.text)
+            self.assertNotIn(self.tag_python_kor.name, post_002_card.text)
             
             post_003_card = main_area.find('div', id='post-3')
             self.assertIn('미분류', post_003_card.text)
             self.assertIn(self.post_003.title, post_003_card.text)
+            self.assertIn(self.post_003.author.username.upper(), post_003_card.text)
+            self.assertNotIn(self.tag_hello.name, post_003_card.text)
+            self.assertIn(self.tag_python.name, post_003_card.text)
+            self.assertIn(self.tag_python_kor.name, post_003_card.text)
             
             self.assertIn(self.user_trump.username.upper(), main_area.text)
             self.assertIn(self.user_obama.username.upper(), main_area.text)
@@ -98,6 +118,8 @@ class TestView(TestCase):
         response= self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
         
         #3. 포스트 목록 페이지와 똑같은 네비게이션 바가 있다.
         self.navbar_test(soup)
@@ -106,9 +128,12 @@ class TestView(TestCase):
         #4. 첫 번째 포스트의 제목이 웹 브라우저 타이틀에 들어있다.
         self.assertIn(self.post_001.title, soup.title.text)
         
+        self.assertIn(self.tag_hello.name, post_area.text)
+        self.assertNotIn(self.tag_python.name, post_area.text)
+        self.assertNotIn(self.tag_python_kor.name, post_area.text)
+        
         #5. 첫 번째 포스트의 제목이 포스트 영역에 있다.
-        main_area = soup.find('div', id='main-area')
-        post_area = main_area.find('div', id='post-area')
+       
         self.assertIn(self.post_001.title, post_area.text)
         self.assertIn(self.category_programming.name, post_area.text)
         
