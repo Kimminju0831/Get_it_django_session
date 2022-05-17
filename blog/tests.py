b3d7fb1b85b3b0515fe5c58a1f626926eedb81a7
Kimminju0831 <@.com>
2022-05-17 16:59:07 +0900

    태그 페이지 만들기

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,4 @@
+from threading import main_thread
 from unicodedata import category
 from urllib import response
 from django.test import TestCase, Client
@@ -167,3 +168,20 @@ class TestView(TestCase):
             
             about_me_btn = navbar.find('a', text='About Me')
             self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+            
+    def test_tag_page(self):
+        response = self.client.get(self.tag_hello.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+        
+        self.assertIn(self.tag_hello.name, soup.h1.text)
+        
+        main_area = soup.find('div', id = 'main-area')
+        self.assertIn(self.tag_hello.name, main_area.text)
+        
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertNotIn(self.post_003.title, main_area.text)
