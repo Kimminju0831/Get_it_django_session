3f48b4aac3d138b6dcd805cdd05f8ef7d869c95e
Kimminju0831 <@.com>
2022-05-22 00:15:52 +0900

    test

--- single_pages/views.py
+++ single_pages/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 def landing(request):
     return render(
         request,
-        'single_pages/landing.html'
+        './single_pages/landing.html'
     )
     
 def about_me(request):
     return render(
         request,
-        'single_pages/about_me.html'
+        './single_pages/about_me.html'
     )
